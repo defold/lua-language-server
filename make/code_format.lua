c6e213e9509d579241c829a5d7303e61866f2455
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-12-20 00:01:20 +0100

    Update code_format.lua

--- make/code_format.lua
+++ make/code_format.lua
@@ -41,6 +41,6 @@ lm:source_set 'code_format' {
                 return "NOT_SUPPORT_FILE_SYSTEM"
             end
         end)(),
-        flags = "-Wall -Werror"
+        flags = "-Wall -Werror -std=gnu99"
     }
 }
